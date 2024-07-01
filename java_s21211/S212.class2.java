6c9b2d1d4f48f7df76b0aefbb33f13322d5f99d9
0taeho0 <166297377+0taeho0@users.noreply.github.com>
2024-07-01 11:44:42 +0900

    Add files via upload

---
+++ java_s21211/S212.class2.java
@@ -0,0 +1,10 @@
+public class S21211_00 {
+   public static void main(String[] args) {
+      int sum = 0;
+      for (int cnt = 1; cnt < 11; cnt++) {
+         sum = sum + cnt;
+         System.out.println("sum: " + sum);
+      }
+      System.out.println("sum: " + sum);
+   }
+}
