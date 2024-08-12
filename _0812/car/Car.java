625bee7d572bca8dd73d45df0a01c6125c8f8468
0taeho0 <166297377+0taeho0@users.noreply.github.com>
2024-08-12 12:21:37 +0900

    Create Car.java

---
+++ _0812/car/Car.java
@@ -0,0 +1,28 @@
+package car;
+
+import tire.Tire;
+
+
+public class Car {
+	//필드
+	public Tire frontLeftTire = new Tire("앞왼쪽", 6);
+	public Tire frontRightTire = new Tire("앞오른쪽", 2);
+	public Tire backLeftTire = new Tire("뒤왼쪽", 3);
+	public Tire backRightTire = new Tire("뒤오른쪽", 4);
+	//생성자
+	//메소드
+	
+	public int run() {
+		System.out.println("[자동차가 달립니다.]");
+		
+		if(frontLeftTire.roll() == false) {stop(); return 1;};
+		if(frontRightTire.roll() == false) {stop(); return 2;};
+		if(backLeftTire.roll() == false) {stop(); return 3;};
+		if(backRightTire.roll() == false) {stop(); return 4;};
+		return 0;
+	}
+
+	void stop() {
+		System.out.println("[자동차가 멈춥니다.]");
+	}
+}
